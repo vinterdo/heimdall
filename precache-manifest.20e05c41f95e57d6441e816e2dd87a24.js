fa7cb01309b67d4f8cf0c3541a958761a7373076
Piotr Brzeźny <pbrzezny@opera.com>
2020-01-08 18:36:59 +0100

    Updates

---
+++ precache-manifest.20e05c41f95e57d6441e816e2dd87a24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d07b7302837f5375ebb760c5a4b43f8",
+    "url": "/heimdall/index.html"
+  },
+  {
+    "revision": "d78dcbc29f4057e957c1",
+    "url": "/heimdall/static/js/2.9da58296.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/heimdall/static/js/2.9da58296.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb6d780cc000a91c7720",
+    "url": "/heimdall/static/js/main.9c3282fc.chunk.js"
+  },
+  {
+    "revision": "f7f5f684aff82ef0ec94",
+    "url": "/heimdall/static/js/runtime-main.816454ba.js"
+  }
+]);
